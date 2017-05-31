8c60b7551bd49492a93eeea4c6d9266d583b0f92
INGPstakra <krawczykstanislaw02@gmail.com>
2017-05-31 12:48:43 +0200

    Add files via upload

---
+++ BombMan/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace BombMan
+{
+    partial class BombMan
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Wymagana metoda obsługi projektanta — nie należy modyfikować 
+        /// zawartość tej metody z edytorem kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BombMan));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.BackgroundImage = global::BombMan.Properties.Resources.Tło_gry_trawa;
+            this.panel1.Controls.Add(this.player);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(390, 330);
+            this.panel1.TabIndex = 0;
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.Transparent;
+            this.player.Location = new System.Drawing.Point(0, 0);
+            this.player.MaximumSize = new System.Drawing.Size(30, 30);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(20, 30);
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            // 
+            // BombMan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(390, 330);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "BombMan";
+            this.Text = "BombMan";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ BombMan/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BombMan
+{
+    public partial class BombMan : Form
+    {
+        bool right;
+        bool left;
+        bool up;
+        bool down;
+
+        Plan R; //plan mapy
+        Blok B, A = new Blok();
+        int ilosc_bomb = 1;
+        int z = 2;  //zasięg wybuchu
+        int index = 0;
+        Blok[] C = new Blok[8];
+
+        public BombMan()
+        {
+            R = new Plan()
+            {
+                N = 100, ///bloczki do niszczenia(max)
+                P = 2   ///potwory(max)
+            };
+            R.Plano(); //stworzenie mapy
+            B = new Blok();
+            B.Bloczki(R); //zamiania planu na elementy graficzne
+            for (int i = 0; i < B.d; i++) //wyswietlanie
+            {
+                if (B.Tablica[i] != null) Controls.Add(B.Tablica[i]);
+            }
+            InitializeComponent();
+            player.Top = 0;
+            player.Left = 0;
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            if (right == true && index == 1)
+            {
+                player.Image = Properties.Resources.PlayerR;
+
+            }
+            if (left == true && index == 1)
+            {
+                player.Image = Properties.Resources.PlayerL;
+            }
+            if (up == true && index == 1)
+            {
+                player.Image = Properties.Resources.PlayerU;
+            }
+            if (down == true && index == 1)
+            {
+                player.Image = Properties.Resources.PlayerD;
+            }
+            if (down == false && up == false && left==false && right==false && index==0)
+            {
+                player.Image = Properties.Resources.PlayerS;
+            }
+            ///colision plansza
+            right = B.Testr(player, right);
+            left = B.Testl(player, left);
+            up = B.Testt(player, up);
+            down = B.Testb(player, down);
+            ///moving
+            if (right == true)
+            {
+                player.Left += 3;
+            }
+            if (left == true)
+            {
+                player.Left -= 3;
+            }
+            if (up == true)
+            {
+                player.Top -= 3;
+            }
+            if (down == true)
+            {
+                player.Top += 3;
+            }
+        }
+
+        private async void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right)
+            {
+                index++;
+                right = true;
+            }
+            if (e.KeyCode == Keys.Left)
+            {
+                index++;
+                left = true;
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                index++;
+                up = true;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                index++;
+                down = true;
+            }
+            if (e.KeyCode == Keys.ControlKey)
+            {
+                if (ilosc_bomb > 0 ) //ograniczenie do bomb
+                {
+                    if (A.Tablica != null)
+                    {
+                        if (A.Tablica[ilosc_bomb - 1] == null)
+                        {
+                            ilosc_bomb = A.Bomba(player, ilosc_bomb, B); //stworzenie bomby
+                        }
+                    }
+                    else
+                    {
+                        ilosc_bomb = A.Bomba(player, ilosc_bomb, B); //stworzenie bomby
+                    }
+                    if (A.Tablica != null && A.d > ilosc_bomb)
+                    {
+                        panel1.Controls.Add(A.Tablica[ilosc_bomb]); //wyświetlenie bomby
+                        int bum = ilosc_bomb;
+                        await Task.Delay(4000); //opóźnienie 4s
+                        C[bum] = B.Bum(z, R, A.Tablica[bum]); //stworzenie płomienia
+                        if (C[bum]==null)
+                        {
+
+                        }
+                        else if (C[bum].d != 0 && C[bum].Tablica != null)
+                        {
+                            
+                            R.Poprawa(B);
+                            for (int i = 0; i < C[bum].d; i++)
+                            {
+                                if (C[bum].Tablica[i] != null && C[bum].Tablica[i].Name != "bomb" + i.ToString() && C[bum].Tablica[i].Name != "block" + i.ToString()) //wyświetlanie bomby
+                                {
+                                    C[bum].Tablica[i].Visible = true;
+                                    panel1.Controls.Add(C[bum].Tablica[i]);
+                                }
+                            }
+                            await Task.Delay(300); //czas wyświetlenia płomienia
+                            for (int i = 0; i < C[bum].d; i++)
+                            {
+                                if (C[bum].Tablica[i] != null) //niszczenie bloczków
+                                {
+                                    C[bum].Tablica[i].Visible = false;
+                                    C[bum].Tablica[i].Dispose();
+                                    C[bum].Tablica[i] = null;
+                                    
+                                }
+                            }
+                            C[bum] = null;
+                            A.Tablica[bum] = null;
+                            ilosc_bomb++;
+                        }
+                    }
+                }
+            }
+        }
+
+
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right)
+            {
+                index = 0;
+                right = false;
+            }
+            if (e.KeyCode == Keys.Left)
+            {
+                index = 0;
+                left = false;
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                index = 0;
+                up = false;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                index = 0;
+                down = false;
+            }
+            if (e.KeyCode == Keys.ControlKey)
+            {
+            }
+        }
+    }
+}
